08662c5c7997e22489ce16088933805f1e1dc32d
1v1expert <1v1expert@gmail.com>
2018-03-03 06:00:03 +0300

    Fix bugs#8

--- functions/aerodoktor-functions.php
+++ functions/aerodoktor-functions.php
@@ -17,8 +17,11 @@ function enqueue_styles() {
     /**
      * Enqueue css
     */
+    wp_register_style('bootstrap', get_template_directory_url() . '/css/bootstrap.min.css');
+    wp_register_style('tether', get_template_directory_url() . '/css/tether.min.css');
+
     wp_enqueue_style('aerodoktor-style', get_stylesheet_url());
-    wp_enqueue_style('bootstrap', get_template_directory_url() . '/css/bootstrap.min.css');
+    wp_enqueue_style('bootstrap');
     wp_enqueue_style('tether', get_template_directory_url() . '/css/tether.min.css');
 }
 add_action('wp_enqueue_scripts', 'enqueue_styles');
@@ -28,4 +31,4 @@ function enqueue_scripts () {
     wp_enqueue_script('main', get_template_directory_url() . '/js/main.js');
     wp_enqueue_script('modernizr', get_template_directory_url() . '/js/modernizr-2.6.2.min.js');
 }
-add_action('wp_enqueue_scripts','aerodoktor_add_script_function');
+add_action('wp_enqueue_scripts','enqueue_scripts');
--- header.php
+++ header.php
@@ -11,7 +11,7 @@
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
+
 </head>
 <body <?php body_class(); ?>>
     <!-- loader -->
