df392cb893fead433d5f40c0cb9a4017cf584b78
1v1expert <1v1expert@gmail.com>
2018-03-03 03:14:33 +0300

    Add header

--- functions/aerodoktor-functions.php
+++ functions/aerodoktor-functions.php
@@ -6,6 +6,89 @@
 */
 
 /**
+ * Loads theme setup functions.
+ */
+function modality_setup() {
+
+	/**
+ 	* Sets up the content width.
+ 	*/
+	global $content_width;
+	if ( ! isset( $content_width ) ) { $content_width = 1200; }
+
+	/**
+	 * Makes theme available for translation
+	 *
+	 */
+	load_theme_textdomain( 'modality', get_template_directory() . '/languages' );
+
+	/**
+ 	* This theme styles the visual editor with editor-style.css to match the theme style.
+ 	*/
+	add_editor_style();
+
+	/**
+ 	 * Default RSS feed links
+	 */
+	add_theme_support('automatic-feed-links');
+
+	/**
+	 * Let WordPress manage the document title.
+	 */
+	add_theme_support( 'title-tag' );
+
+	/**
+ 	* Register Navigation
+ 	*/
+	register_nav_menu('main_navigation', __('Primary Menu', 'modality') );
+
+	/**
+ 	* Support a variety of post formats.
+ 	*/
+	add_theme_support( 'post-formats', array( 'aside', 'image', 'video', 'audio', 'gallery' ) );
+
+	/**
+ 	* Custom image size for featured images, displayed on "standard" posts.
+ 	*/
+	add_theme_support( 'post-thumbnails' );
+	set_post_thumbnail_size( 2500, 9999 ); // Unlimited height, soft crop.
+
+	/*
+	 * Switch default core markup for search form, comment form, and comments
+	 * to output valid HTML5.
+	 */
+	add_theme_support( 'html5', array(
+		'search-form', 'comment-form', 'comment-list', 'gallery', 'caption',
+	) );
+
+	/**
+ 	* Sets up theme custom background.
+ 	*/
+	add_theme_support( 'custom-background', apply_filters( 'modality_custom_background_args', array(
+		'default-color' => 'fcfcfc',
+		'default-image' => '',
+	) ) );
+
+	/**
+	* Sets up theme custom header image.
+	*/
+	add_theme_support( 'custom-header', apply_filters( 'modality_custom_header_args', array(
+		'width'                  => 1920,
+		'height'                 => 200,
+		'flex-height'            => true,
+		'header-text'            => false,
+	) ) );
+
+	/*
+	* Enable support for custom logo.
+	*/
+	add_image_size( 'modality-logo', 300, 80 );
+	add_theme_support( 'custom-logo', array( 'size' => 'modality-logo' ) );
+}
+
+add_action( 'after_setup_theme', 'modality_setup' );
+
+/**
  * Add scripts function
  */
 function aerodoktor_add_script_function() {
---
+++ header.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * The Header of the theme.
+ *
+ * Displays all of the <head> section and everything up till <main id="main">
+ *
+ * @package Aerodoktor
+ */
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width">
+
+</head>
+<body <?php body_class(); ?>>
+    <!-- loader -->
+    <div id="loader-wrapper">
+        <div id="loader"></div>
+        <div class="loader-section section-left"></div>
+        <div class="loader-section section-right"></div>
+    </div>
+
+    <!-- start top navbar -->
+    <section id="top-nav">
+        <div class="container">
+            <div class="top">
+                <div class="row">
+                    <div class="col-lg-10">
+                        <div class="left">
+                            <ul>
+                                <li>
+                                    <a href="" class="btn btn-link">AeroDoktor</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-2">
+                        <div class="right hidden-md-down">
+                            <ul>
+                                <li>
+                                    <a href="" class="btn btn-link">8 (985) 229-67-56</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- start header -->
+    <div id="header">
+        <div class="container">
+            <div class="head">
+                <div class="row">
+                    <div class="col-lg-3">
+                        <div class="left">
+                            <a href="" class="navbar-brand"><img src="images/logo.png"></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
