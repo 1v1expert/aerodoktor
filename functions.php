cc8611d8ca964e9f49566c66cbe1d27097ccdcee
1v1expert <1v1expert@gmail.com>
2018-03-03 03:47:35 +0300

    Remove files

--- functions.php
+++ functions.php
@@ -9,7 +9,16 @@
  *  Load theme files
 /* ------------------------------------------------------------------------- */
 require_once ('functions/aerodoktor-functions.php');
-require_once ('functions/modality-customizer.php');			// Load Customizer
-require_once ('functions/modality-image-sliders.php'); 		// Theme Custom Functions
-require_once ('functions/modality-woocommerce.php');		// WooCommerce Support
-require_once ('functions/wp_bootstrap_navwalker.php');
+function aerodoktor_add_script_function() {
+    /**
+     * Enqueue css
+    */
+    wp_enqueue_style('aerodoktor-style', get_stylesheet_url());
+    wp_enqueue_style('bootstrap', get_template_directory_url() . '/css/bootstrap.min.css');
+    wp_enqueue_style('tether', get_template_directory_url() . '/css/tether.min.css');
+
+    wp_enqueue_script('jquery', get_template_directory_url() . '/js/jquery-1.9.1.min.js', array( 'jquery' ), '', true);
+    wp_enqueue_script('main', get_template_directory_url() . '/js/main.js', array( 'jquery' ), '', true);
+    wp_enqueue_script('modernizr', get_template_directory_url() . '/js/modernizr-2.6.2.min.js', array( 'jquery' ), '', true);
+
+add_action('wp_enqueue_scripts','aerodoktor_add_script_function');
