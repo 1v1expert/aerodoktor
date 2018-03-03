f8734abba9d019a0cae16582b93e06ec78c45357
1v1expert <1v1expert@gmail.com>
2018-03-04 02:27:53 +0300

    Fix functions in aerodoktor-functions

--- functions/aerodoktor-functions.php
+++ functions/aerodoktor-functions.php
@@ -13,23 +13,22 @@
 /**
  * Add scripts function
  */
-function enqueue_styles() {
+function aerodoktor_add_script_function()() {
     /**
      * Enqueue css
     */
     wp_register_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css');
     wp_register_style('tether', get_template_directory_uri() . '/css/tether.min.css');
 
-    wp_enqueue_style('aerodoktor-style', get_stylesheet_uri());
     wp_enqueue_style('bootstrap');
     wp_enqueue_style('tether');
-}
-add_action('wp_enqueue_scripts', 'enqueue_styles');
-
-function enqueue_scripts () {
+    wp_enqueue_style('aerodoktor-style', get_stylesheet_uri());
+    /**
+	 * Enqueue javascripts
+	 */
     wp_register_script('jquery-dop', get_template_directory_uri() . '/js/jquery-1.9.1.min.js');
     wp_enqueue_script('jquery-dop');
     wp_enqueue_script('modernizr', get_template_directory_uri() . '/js/modernizr-2.6.2.min.js');
     wp_enqueue_script('main', get_template_directory_uri() . '/js/main.js');
 }
-add_action('wp_enqueue_scripts','enqueue_scripts');
+add_action('wp_enqueue_scripts','aerodoktor_add_script_function');
