ff87659a07c42835b5cc37738a5069552dade2e3
1v1expert <1v1expert@gmail.com>
2018-03-03 05:02:56 +0300

    Fix bugs#4

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Aerodoktor functions and defenitions
+ *
+ * @package Aerodoktor
+*/
+
+/* ------------------------------------------------------------------------- *
+ *  Load theme files
+/* ------------------------------------------------------------------------- */
+require_once ('functions/aerodoktor-functions.php');
--- functions/aerodoktor-functions.php
+++ functions/aerodoktor-functions.php
@@ -13,16 +13,19 @@
 /**
  * Add scripts function
  */
-function aerodoktor_add_script_function() {
+function enqueue_styles() {
     /**
      * Enqueue css
     */
     wp_enqueue_style('aerodoktor-style', get_stylesheet_url());
     wp_enqueue_style('bootstrap', get_template_directory_url() . '/css/bootstrap.min.css');
     wp_enqueue_style('tether', get_template_directory_url() . '/css/tether.min.css');
+}
+add_action('wp_enqueue_scripts', 'enqueue_styles');
 
-    wp_enqueue_script('jquery', get_template_directory_url() . '/js/jquery-1.9.1.min.js', array( 'jquery' ), '', true);
-    wp_enqueue_script('main', get_template_directory_url() . '/js/main.js', array( 'jquery' ), '', true);
-    wp_enqueue_script('modernizr', get_template_directory_url() . '/js/modernizr-2.6.2.min.js', array( 'jquery' ), '', true);
+function enqueue_scripts () {
+    wp_enqueue_script('jquery', get_template_directory_url() . '/js/jquery-1.9.1.min.js');
+    wp_enqueue_script('main', get_template_directory_url() . '/js/main.js');
+    wp_enqueue_script('modernizr', get_template_directory_url() . '/js/modernizr-2.6.2.min.js');
 
 add_action('wp_enqueue_scripts','aerodoktor_add_script_function');
