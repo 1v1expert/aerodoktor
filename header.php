8a6c8fe629ecf6832c0036ad668620bb38322008
1v1expert <1v1expert@gmail.com>
2018-03-03 04:36:12 +0300

    fix bugs

--- header.php
+++ header.php
@@ -1,13 +1,5 @@
-<?php
-/**
- * The Header of the theme.
- *
- * Displays all of the <head> section and everything up till <main id="main">
- *
- * @package Aerodoktor
- */
-?><!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+<!DOCTYPE html>
+<html>
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
