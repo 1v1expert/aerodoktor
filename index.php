8ad031f36edcb0ce24c656ef24262581baf2443d
1v1expert <1v1expert@gmail.com>
2018-03-05 04:00:12 +0300

    Fix bugs in index page

--- index.php
+++ index.php
@@ -7,5 +7,21 @@
  * @package Aerodoktor
  */
 get_header(); ?>
-<h2> Index.php </h2>
+
+<section id="what">
+<div class="container">
+  <div class="row">
+    <div class="col-lg-12">
+      <div class="section-head">
+        <h1>Сайт в разработке</h1>
+        <img src="etc/img/line.png" alt="Logo">
+        <p>
+          Пожалуйста, не уходите. Спасибо за понимание
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
+</section>
+
 <?php get_footer(); ?>
--- page.php
+++ page.php
@@ -6,20 +6,6 @@
  */
  get_header(); ?>
 
-  <section id="what">
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-12">
-          <div class="section-head">
-            <h1>Сайт в разработке</h1>
-            <img src="etc/img/line.png" alt="Logo">
-            <p>
-              Пожалуйста, не уходите. Спасибо за понимание
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
+<h2> Page.php </h2>
 
  <?php get_footer(); ?>
