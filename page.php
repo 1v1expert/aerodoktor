1b1dee7d3b8b4cfa9ff552ad0307bfb4946f2771
1v1expert <1v1expert@gmail.com>
2018-03-05 04:05:38 +0300

    Rewrite page.php

--- page.php
+++ page.php
@@ -6,6 +6,20 @@
  */
  get_header(); ?>
 
-<h2> Page.php </h2>
+<section id="what">
+<div class="container">
+  <div class="row">
+    <div class="col-lg-12">
+      <div class="section-head">
+        <h1>Сайт в разработке</h1>
+        <img src="etc/img/line.png" alt="Logo">
+        <p>
+          Пожалуйста, не уходите. Спасибо за понимание
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
+</section>
 
  <?php get_footer(); ?>
